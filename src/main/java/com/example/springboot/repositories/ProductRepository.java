2d32e3593bb4b8dccc9bf5fccc37d9df57fa104d
Cauã Andrade <caualsantos@hotmail.com>
2023-09-05 01:01:50 -0300

    Adição da filtragem por nome

--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -1,13 +1,19 @@
 package com.example.springboot.repositories;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.springboot.models.ProductModel;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+    @Query(value = "select p from ProductModel p where p.name like %?1%")
+    Optional<List<ProductModel>> productsName(String name);
 
 }
